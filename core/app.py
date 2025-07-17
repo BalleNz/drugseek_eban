6221b5ea96c44ad451aa3ccb070bd603fcc2bd15
Арсен Зиядинов <>
2025-07-17 18:47:38 +0300

    fastapi app

--- core/app.py
+++ core/app.py
@@ -1,8 +1,22 @@
 import FastAPI
+import uvicorn
 
-def get_app():
+from handlers.drug import drug_router
+
+
+def get_app() -> FastAPI:
     app = FastAPI(
         title="DrugSearch API"
     )
 
-    
+    app.include_router(drug_router, prefix="/drugs", tags=["drugs"])
+    return app
+
+app = get_app()
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app=app,
+        host=...,
+        port=...,
+    )

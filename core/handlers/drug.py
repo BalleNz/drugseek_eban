a4c22cf596368aea1afdf75ee8e2f252491ce825
Арсен Зиядинов <>
2025-07-23 02:45:44 +0300

    ref: handlers

--- core/handlers/drug.py
+++ core/handlers/drug.py
@@ -4,7 +4,9 @@ from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.params import Path
 
+from assistant import assistant
 from database.models.user import User
+from schemas.assistant_responses import AssistantResponseDrugValidation, STATUS
 from schemas.drug_schemas import DrugSchema
 from schemas.response import DrugResponse
 from services.drug import DrugService, get_drug_service
@@ -18,13 +20,23 @@ async def new_drug(
         user_query: Path(),
         user: User = ...,
         drug_service: DrugService = Depends(get_drug_service),
-) -> Union[DrugSchema, HTTPException]:
+        user_service: UserService = Depends(get_user_service)
+) -> Union[AssistantResponseDrugValidation, HTTPException]:
     # TODO:
     # 1. Neuro_response: {"status": "exist/not exist", "drug_name":"..."}
     # 2. Отнять токен (если валидный статус)
     # 3. Create drug
     # 3. Allow drug
     # 4. Return drug
+    assistant_response: AssistantResponseDrugValidation = assistant.get_user_query_validation(user_query=user_query)
+    if assistant_response.status == STATUS.NOT_EXIST:
+        return assistant_response
+
+    if assistant_response.status == STATUS.EXIST:
+        if user.allowed_requests:
+            await drug_service.create_drug(assistant_response.drug_name)
+            await user_service.reduce_token(user)
+            await user_service.allow_drug_to_user(user=user, drug_id=...)
     ...
 
 
@@ -34,6 +46,7 @@ async def search_and_allow_request(
         user: User = ...,  # TODO: auth with O2auth schema via telegram
         drug_service: DrugService = Depends(get_drug_service)
 ):
+    """ПОИСК ПО ТРИГРАММАМ"""
     # TODO:
     # 1. Если найден ->
     # 2. Отнять токен
--- core/handlers/user.py
+++ core/handlers/user.py
@@ -9,15 +9,6 @@ from services.user import UserService, get_user_service
 user_router = APIRouter(prefix="/user", tags=["User"])
 
 
-@user_router.post(path="/allow_drug/{drug_id}")
-async def allow_drug_to_user(
-        user: User = ...,  # TODO: auth
-        drug_id: UUID = Path(),
-        user_service: UserService = Depends(get_user_service),
-):
-    await user_service.allow_drug_to_user(user=user, drug_id=drug_id)
-
-
 @user_router.get(path="/", response_model=User)
 async def get_me(
         user: User = ...

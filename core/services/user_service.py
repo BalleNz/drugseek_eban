3fa91fd4f4a0494f2fa1e3aac147b2fb0dd49b8c
Арсен Зиядинов <>
2025-07-31 15:56:44 +0300

    feat: user_description generation via assistant

--- core/services/user_service.py
+++ core/services/user_service.py
@@ -2,6 +2,7 @@ from uuid import UUID
 
 from fastapi import Depends
 
+from assistant import assistant
 from database.repository.user import UserRepository, get_user_repository
 from schemas.user import UserTelegramDataSchema, UserSchema
 
@@ -14,7 +15,14 @@ class UserService:
         return await self.repo.allow_drug_to_user(user_id=user_id, drug_id=drug_id)
 
     async def update_user_description(self, user_id: UUID):
-        pass
+        user: UserSchema = await self.repo.get(user_id)
+        user_drugs = await self.repo.get_allowed_drug_names(user_id=user.id)
+
+        user_description = assistant.get_user_description(
+            user_name=user.first_name + user.last_name,
+            user_drug_names=user_drugs
+        )
+        await self.repo.update_user_description(description=user_description, user_id=user.id)
 
 
 def get_user_service(repo: UserRepository = Depends(get_user_repository)) -> UserService:
--- tests/test_service/test_user_service.py
+++ tests/test_service/test_user_service.py
@@ -1,6 +1,46 @@
+import random
+import uuid
+
 import pytest
 
 from database.models.user import User
 from schemas import UserSchema, UserTelegramDataSchema
+from test_repository.test_drug_repo import create_test_drug_model
+from test_repository.test_user_repo import get_user
 
+
+@pytest.mark.asyncio
+async def test_get_user_description_via_assistant(user_service, drug_repo):
+    user = get_user()
+    await user_service.repo.create(user)
+    await user_service.repo._session.refresh(user)
+
+    # добавление препаратов
+    drug_names_ru = [
+        "роаккутан",
+        "теанин",
+        "Тренболон",
+        "Тестостерон",
+        "ук11",
+        "тадалафил",
+        "виагра",
+        "кленбутерол",
+    ]
+    drugs = [create_test_drug_model() for _ in range(8)]
+    for drug in drugs:
+        drug.name_ru = drug_names_ru.pop()
+
+        drug.name = str(uuid.uuid4())  # to evade unique field
+        drug.analogs = []
+        drug.pathways = []
+        drug.synonyms = []
+        drug.combinations = []
+
+        await drug_repo.create(drug)
+        await user_service.allow_drug_to_user(user_id=user.id, drug_id=drug.id)
+
+    await user_service.update_user_description(user.id)
+    await user_service.repo._session.refresh(user)
+
+    assert user.description
 
